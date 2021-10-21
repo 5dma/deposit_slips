bcdf693982794f13f85d643f8b16099adfd68769
5dma <mark@lautman.net>
2021-10-20 20:43:57 -0400

    Checkbox working

--- app_activate.c
+++ app_activate.c
@@ -1,19 +1,13 @@
 #include <gtk/gtk.h>
-
+/* G_MESSAGES_DEBUG=all ./deposit_slip */
 // callback function which is called when button is clicked
-static void on_button_clicked(GtkButton *btn, gpointer data)
-{
+static void on_button_clicked(GtkButton *btn, gpointer data) {
     // change button label when it's clicked
     gtk_button_set_label(btn, "Hello World");
 }
 
-static void barf(GApplication *app, gpointer data)
-{
-    g_print("OMG barf");
-}
 
-enum
-{
+enum {
     ACCOUNT_NUMBER,
     DESCRIPTION,
     CHECKBOX,
@@ -24,22 +18,16 @@ void last_row_cell_data_func(GtkTreeViewColumn *col,
                              GtkCellRenderer *renderer,
                              GtkTreeModel *model,
                              GtkTreeIter *iter,
-                             gpointer user_data)
-{
-
+                             gpointer user_data) {
     gchararray account_number[500];
     gtk_tree_model_get(model, iter, ACCOUNT_NUMBER, account_number, -1);
     g_print("Here is the account number after %s\n", *account_number);
 
-    if (g_ascii_strcasecmp("New", *account_number) == 0)
-    {
+    if (g_ascii_strcasecmp("New", *account_number) == 0) {
         g_object_set(renderer, "style", PANGO_STYLE_ITALIC, NULL);
-    }
-    else
-    {
+    } else {
         g_object_set(renderer, "style", PANGO_STYLE_NORMAL, NULL);
     }
-
     g_object_set(renderer, "text", *account_number, NULL);
 }
 
@@ -57,9 +45,37 @@ static void account_number_edited(GtkCellRendererText *renderer,
     }
 }
 
+static void description_edited(GtkCellRendererText *renderer,
+                               gchar *path,
+                               gchar *new_description,
+                               GtkTreeView *treeview) {
+    GtkTreeIter iter;
+    GtkTreeModel *model;
+    if (g_ascii_strcasecmp(new_description, "") != 0) {
+        model = gtk_tree_view_get_model(treeview);
+        if (gtk_tree_model_get_iter_from_string(model, &iter, path)) {
+            gtk_list_store_set(GTK_LIST_STORE(model), &iter, DESCRIPTION, new_description, -1);
+        }
+    }
+}
+
+static void toggle_clicked(GtkCellRendererToggle *renderer,
+                           gchar *path,
+                           GtkTreeView *treeview) {
+    GtkTreeIter iter;
+    GtkTreeModel *model;
+    gboolean value;
+    model = gtk_tree_view_get_model(treeview);
+    g_debug("Here\n");
+    if (gtk_tree_model_get_iter_from_string(model, &iter, path)) {
+        gtk_tree_model_get(model, &iter, CHECKBOX, &value, -1);
+        gtk_list_store_set(GTK_LIST_STORE(model), &iter, CHECKBOX, !value, -1);
+        
+    }
+}
+
 // callback function which is called when application is first started
-void on_app_activate(GApplication *app, gpointer data)
-{
+void on_app_activate(GApplication *app, gpointer data) {
 
     GtkWidget *window = gtk_application_window_new(GTK_APPLICATION(app));
 
@@ -73,10 +89,6 @@ void on_app_activate(GApplication *app, gpointer data)
 
     gtk_notebook_append_page(GTK_NOTEBOOK(notebook), vbox_slip, label_slip);
     gtk_notebook_append_page(GTK_NOTEBOOK(notebook), vbox_accounts, label_account);
-
-    GtkWidget *btn = gtk_button_new_with_label("Click Me!");
-
-    g_signal_connect(btn, "clicked", G_CALLBACK(on_button_clicked), NULL);
 
     GtkListStore *list_store = gtk_list_store_new(N_COLUMNS, G_TYPE_STRING, G_TYPE_STRING, G_TYPE_BOOLEAN);
 
@@ -124,21 +136,20 @@ void on_app_activate(GApplication *app, gpointer data)
                                                                  "text", DESCRIPTION,
                                                                  NULL);
 
+    g_object_set(rendererDescription, "editable", TRUE, "editable-set", TRUE, NULL);
+    g_signal_connect(G_OBJECT(rendererDescription), "edited", G_CALLBACK(description_edited), (gpointer)tree);
+
     GtkCellRenderer *rendererToggle;
     GtkTreeViewColumn *columnToggle;
     rendererToggle = gtk_cell_renderer_toggle_new();
 
     columnToggle = gtk_tree_view_column_new_with_attributes("Include",
                                                             rendererToggle,
-                                                            "activatable", CHECKBOX,
+                                                            "active", CHECKBOX,
                                                             NULL);
 
-    //  GtkCellRenderer *rendererAddAccount;
-    //  rendererAddAccount = gtk_cell_renderer_text_new();
-    columnToggle = gtk_tree_view_column_new_with_attributes("Include",
-                                                            rendererToggle,
-                                                            "activatable", CHECKBOX,
-                                                            NULL);
+    g_object_set(rendererToggle, "activatable", TRUE, "active", FALSE, NULL);
+    g_signal_connect(G_OBJECT(rendererToggle), "toggled", G_CALLBACK(toggle_clicked), (gpointer)tree);
 
     gtk_tree_view_append_column(GTK_TREE_VIEW(tree), columnAccount);
     gtk_tree_view_append_column(GTK_TREE_VIEW(tree), columnDescription);
@@ -146,14 +157,11 @@ void on_app_activate(GApplication *app, gpointer data)
 
     gtk_tree_view_column_set_cell_data_func(columnAccount, rendererAccount, last_row_cell_data_func, NULL, NULL);
 
-    g_signal_connect(rendererToggle, "toggled", G_CALLBACK(barf), NULL);
-
     g_object_unref(list_store); /* destroy model automatically with view */
 
     GtkWidget *topBox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 3);
 
     gtk_box_pack_start(GTK_BOX(vbox_accounts), tree, FALSE, FALSE, 0);
-    gtk_box_pack_start(GTK_BOX(topBox), tree, FALSE, FALSE, 0);
 
     gtk_container_add(GTK_CONTAINER(window), notebook);
 
