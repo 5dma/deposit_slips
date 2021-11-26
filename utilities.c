2c9ee6920a62aea0d1a3011818f694654ff0b52b
5dma <mark@lautman.net>
2021-11-26 12:44:52 -0500

    Freeing memory when application is destroyed.

--- utilities.c
+++ utilities.c
@@ -1,4 +1,5 @@
 #include <gtk/gtk.h>
+
 #include "headers.h"
 
 /**
@@ -12,24 +13,44 @@
     @param child_name Name of the child node.
     @return A widget corresponding to the passed name of the child node.
 */
-GtkWidget *get_child_from_parent(GtkWidget *parent, const gchar* child_name) {
-
+GtkWidget *get_child_from_parent(GtkWidget *parent, const gchar *child_name) {
     GList *children_list = gtk_container_get_children(GTK_CONTAINER(parent));
 
     do {
-        if (g_strcmp0 ( gtk_widget_get_name(children_list->data),child_name) == 0) {
+        if (g_strcmp0(gtk_widget_get_name(children_list->data), child_name) == 0) {
             return children_list->data;
         }
     } while ((children_list = g_list_next(children_list)) != NULL);
-
-   
 }
 
-void free_memory (GtkWidget *window, gpointer user_data) {
-     GPtrArray *list_store_struct = (GPtrArray *) user_data;
-   
-    g_print("All over\n");
-
+/**
+    Frees memory a GSList of accounts. Called from `free_memory`.
+    @param data Pointer to user data, which is to an Account entry in a GSList.
+*/
+GDestroyNotify free_gslist_account(gpointer data) {
+    Account *account = (Account *)data;
+    g_free(account);
 }
 
 
+/**
+    Frees memory in the master and temporary account lists. This callback fires after
+    the user destroyes the main application windows.
+    @param window The parent node.
+    @param user_data Pointer to user data
+    @return A widget corresponding to the passed name of the child node.
+*/
+void free_memory(GtkWidget *window, gpointer user_data) {
+    GPtrArray *list_store_struct = (GPtrArray *)user_data;
+    GSList *master_account_list = g_ptr_array_index(list_store_struct, POSITION_LIST_STORE_MASTER);
+    g_slist_free_full(master_account_list, (GDestroyNotify)free_gslist_account);
+     GSList *temporary_account_list = g_ptr_array_index(list_store_struct, POSITION_LIST_STORE_TEMPORARY);
+    g_slist_free_full(temporary_account_list, (GDestroyNotify)free_gslist_account);
+
+    /* The following lines clear the list_store behind the store of the account tree. However
+    it gives us a run-time error, it seems like the store no longer exists.
+    List_Builder_Struct *list_builder_struct = g_ptr_array_index(list_store_struct, POSITION_LIST_BUILDER_STRUCT);
+    GtkListStore *list_store =  list_builder_struct->list_store;
+    gtk_list_store_clear (list_store);
+    */
+}
